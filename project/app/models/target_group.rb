dc9f164229210af16d5e2acec42889f842956d5f
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-21 15:29:52 +0200

    Fix class_name in parent and children associations of TargetGroup model

--- project/app/models/target_group.rb
+++ project/app/models/target_group.rb
@@ -1,8 +1,8 @@
 class TargetGroup < ApplicationRecord
   belongs_to :panel_provider
-  belongs_to :parent, optional: true, class_name: self.class.name, foreign_key: "parent_id"
+  belongs_to :parent, optional: true, class_name: name, foreign_key: "parent_id"
   has_and_belongs_to_many :countries
-  has_many :children, class_name: self.class.name, foreign_key: "parent_id"
+  has_many :children, class_name: name, foreign_key: "parent_id"
 
   validates :external_id, :name, :secret_code, presence: true
 end
