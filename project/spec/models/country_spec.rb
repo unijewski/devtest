2aac8d2092cfd451add55b4eb7ea885cef7272fd
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-21 14:21:12 +0200

    Update specs of Country model

--- project/spec/models/country_spec.rb
+++ project/spec/models/country_spec.rb
@@ -3,11 +3,14 @@ require "rails_helper"
 describe Country, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:panel_provider) }
+    it { is_expected.to have_and_belong_to_many(:target_groups).conditions("parent_id IS NULL") }
     it { is_expected.to have_many(:location_groups) }
   end
 
   describe "validations" do
+    subject { build(:country) }
+
     it { is_expected.to validate_presence_of(:code) }
-    it { is_expected.to validate_presence_of(:panel_provider) }
+    it { is_expected.to validate_uniqueness_of(:code) }
   end
 end
