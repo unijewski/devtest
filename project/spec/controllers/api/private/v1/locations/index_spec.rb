d997283e7a767dd7009c25345f3172ab61160c61
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 22:41:03 +0200

    Add specs of locations endpoints

---
+++ project/spec/controllers/api/private/v1/locations/index_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe API::Private::V1::Locations::Index, type: :request do
+  describe "GET /api/private/v1/locations" do
+    subject { get "/api/private/v1/locations", params: params }
+
+    context "when valid params" do
+      let(:params) { { country_code: "PL" } }
+
+      it "responds with success status" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "invokes GetLocationByCountryCodeQuery" do
+        expect_any_instance_of(GetLocationByCountryCodeQuery).to receive(:call).and_call_original
+        subject
+      end
+    end
+
+    context "when invalid params" do
+      let(:params) { {} }
+
+      it "responds with bad request status" do
+        subject
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it "does not invoke GetLocationByCountryCodeQuery" do
+        expect_any_instance_of(GetLocationByCountryCodeQuery).to_not receive(:call)
+        subject
+      end
+    end
+  end
+end
