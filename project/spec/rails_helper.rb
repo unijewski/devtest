d6cc043d21ed450f7f7e13a050d4296834d09038
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 16:26:07 +0200

    Configure webmock with vcr in rails_helper

--- project/spec/rails_helper.rb
+++ project/spec/rails_helper.rb
@@ -6,6 +6,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'vcr'
+require 'webmock/rspec'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -84,4 +87,9 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
 end
