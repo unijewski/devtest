cdc116bc6ab1e22bcbf5ca137fe60b3124bd3f3a
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 22:42:55 +0200

    Add specs of target_groups endpoints

---
+++ project/spec/controllers/api/private/v1/target_groups/index_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe API::Private::V1::TargetGroups::Index, type: :request do
+  describe "GET /api/private/v1/target_groups" do
+    subject { get "/api/private/v1/target_groups", params: params }
+
+    context "when valid params" do
+      let(:params) { { country_code: "PL" } }
+
+      it "responds with success status" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "invokes GetTargetGroupByCountryCodeQuery" do
+        expect_any_instance_of(GetTargetGroupByCountryCodeQuery).to receive(:call).and_call_original
+        subject
+      end
+    end
+
+    context "when invalid params" do
+      let(:params) { {} }
+
+      it "responds with bad request status" do
+        subject
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it "does not invoke GetTargetGroupByCountryCodeQuery" do
+        expect_any_instance_of(GetTargetGroupByCountryCodeQuery).to_not receive(:call)
+        subject
+      end
+    end
+  end
+end
