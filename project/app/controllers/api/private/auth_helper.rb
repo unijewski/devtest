4557edbb13435d7f0d0e771f9b78da11443e93fc
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 23:19:15 +0200

    Create API AuthHelper

---
+++ project/app/controllers/api/private/auth_helper.rb
@@ -0,0 +1,24 @@
+module API
+  module Private
+    module AuthHelper
+      extend ::Grape::API::Helpers
+
+      def authenticate!
+        return if access_token && access_token_valid?
+
+        error!("Unauthorized", 401)
+      end
+
+      def access_token
+        headers["Authorization"]
+      end
+
+      def access_token_valid?
+        ActiveSupport::SecurityUtils.secure_compare(
+          headers["Authorization"],
+          Rails.application.credentials.api_access_token
+        )
+      end
+    end
+  end
+end
