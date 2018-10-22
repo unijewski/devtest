2b93c85632ab382696e24567783ca871800153ad
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 22:03:33 +0200

    Use SecureRandom methods in Location factory

--- project/spec/factories/locations.rb
+++ project/spec/factories/locations.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :location do
     name { "Krakow" }
-    external_id { "KR" }
-    secret_code { "1b3d78d7-d375-4998-84b4-b17c54018662" }
+    external_id { SecureRandom.uuid }
+    secret_code { SecureRandom.hex(64) }
   end
 end
