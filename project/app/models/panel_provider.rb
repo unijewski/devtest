809e5dd1ff663eca21937c067d8f70b7824ae130
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-10-22 17:20:02 +0200

    Add PanelProvider#price method

--- project/app/models/panel_provider.rb
+++ project/app/models/panel_provider.rb
@@ -2,4 +2,8 @@ class PanelProvider < ApplicationRecord
   has_many :countries
 
   validates :code, presence: true, uniqueness: true
+
+  def price
+    PanelProviderServices::FetchPrice.new(code: code).call
+  end
 end
--- project/spec/models/panel_provider_spec.rb
+++ project/spec/models/panel_provider_spec.rb
@@ -11,4 +11,23 @@ describe PanelProvider, type: :model do
     it { is_expected.to validate_presence_of(:code) }
     it { is_expected.to validate_uniqueness_of(:code) }
   end
+
+  describe "#price" do
+    let(:service_instance) { double("PanelProviderServices::FetchPrice") }
+
+    subject { build_stubbed(:panel_provider).price }
+
+    before do
+      allow(PanelProviderServices::FetchPrice).to receive(:new).and_return(service_instance)
+    end
+
+    it "invokes PanelProviderServices::FetchPrice service" do
+      expect(PanelProviderServices::FetchPrice)
+        .to receive(:new)
+        .with(code: "times_a")
+        .and_return(service_instance)
+      expect(service_instance).to receive(:call)
+      subject
+    end
+  end
 end
